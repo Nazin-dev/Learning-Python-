7e54b6641324475f03bfc76f4ba0a6f53ffbe615
Nailton Reis <nazin.dev@gmail.com>
2024-08-04 21:52:27 -0300

    Introdução ao empacotamento, desempacotamento e tuplas

---
+++ aula36.py
@@ -0,0 +1,9 @@
+"""
+Introdução ao empacotamento, desempacotamento e tuplas 
+"""
+nome1, *resto = ['Luisa', 'Henri', 'Pedro']
+# Estou colocando o primeiro valor na variavel nome1 e os outros valores na variavel resto
+
+nomes = 'Luisa', 'Henri', 'Pedro' # Isso é uma tupla, no caso um tipo imutavel 
+
+print(resto)
